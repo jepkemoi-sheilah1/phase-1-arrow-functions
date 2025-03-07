64ad9b7d522f2b061ef2c7b11d77539a43454c24
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-07 05:48:24 +0300

    add arrow function to complrete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+//let divide = (a,b) => {
+  //  2000/100;
+    
+//}
+  // let square = (x) => { 
+    //x * x;
+    //(x//)=4;
+    
+   //}
+   //let add = (a,b) => {
+    //a + b;
+    //(4+4)=7;
+   //}
+   const divide = () => 2000 / 100;
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+   
